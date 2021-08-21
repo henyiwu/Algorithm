0cc0052b6fcbed09964d678bf70f39e04cf77056
wangzhiping <wangzhiping@inke.cn>
2021-08-21 23:42:24 +0800

    [两数之合] o n 解法

---
+++ src/zuochengyun/book/chapter_1/MaxTree.java
@@ -0,0 +1,17 @@
+package zuochengyun.book.chapter_1;
+
+public class MaxTree {
+
+
+
+}
+
+class Node {
+    public int value;
+    public Node left;
+    public Node right;
+
+    public Node(int data) {
+        this.value = data;
+    }
+}
