98e304179659e56dd31d05e31eaf71bc83224f21
henyiwu <wang921332617@qq.com>
2021-09-04 23:13:38 +0800

    [leetcode21]

---
+++ src/leetcode/LeetCode_21.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+class ListNode21 {
+    int val;
+    ListNode next;
+    ListNode21() {}
+    ListNode21(int val) { this.val = val; }
+    ListNode21(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+
+public class LeetCode_21 {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l2 == null) {
+            return l1;
+        }
+        if (l1 == null) {
+            return l2;
+        }
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+}
