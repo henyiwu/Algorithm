223837ec9d74f703618b514fafe2f39bae4fb1d9
henyiwu <wang921332617@qq.com>
2021-09-05 22:49:16 +0800

    [leetcode 53]

---
+++ src/leetcode/LeetCode_53.java
@@ -0,0 +1,47 @@
+package leetcode;
+
+/**
+ * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+ * 输出：6
+ * 解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
+ * 示例 2：
+ *
+ * 输入：nums = [1]
+ * 输出：1
+ * 示例 3：
+ *
+ * 输入：nums = [0]
+ * 输出：0
+ * 示例 4：
+ *
+ * 输入：nums = [-1]
+ * 输出：-1
+ * 示例 5：
+ *
+ * 输入：nums = [-100000]
+ * 输出：-100000
+ */
+public class LeetCode_53 {
+    public static void main(String[] args) {
+        System.out.println(maxSubArray(new int[] {-2,1,-3}));
+    }
+
+    /**
+     [-2,1,-3]
+     */
+    public static int maxSubArray(int[] nums) {
+        int preMax = 0;
+        int curMax = nums[0];
+        for (int item : nums) {
+            preMax = Math.max(item, item + preMax);
+            curMax = Math.max(curMax, preMax);
+        }
+        return curMax;
+    }
+}
