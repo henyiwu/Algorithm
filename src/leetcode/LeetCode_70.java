34f8df7cbf88d36ce6b8a2c6c1cb2302866a0bee
henyiwu <wang921332617@qq.com>
2021-10-30 21:09:10 +0800

    [leetcode]

--- src/leetcode/LeetCode_70.java
+++ src/leetcode/LeetCode_70.java
@@ -27,7 +27,7 @@ package leetcode;
  * 链接：https://leetcode-cn.com/problems/climbing-stairs
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
-public class LeetCode {
+public class LeetCode_70 {
 
     public int climbStairs(int n) {
         //f(1) = 1
