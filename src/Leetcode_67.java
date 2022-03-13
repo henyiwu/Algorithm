2e89e209088b3f98d09e9b3d22aee1721eb94d95
wangzhiping <wang921332617@qq.com>
2022-03-13 22:46:39 +0800

    leetcode88

--- src/Leetcode_67.java
+++ src/Leetcode_67.java
@@ -29,19 +29,19 @@ public class Leetcode_67 {
 
 class Solution {
     public String addBinary(String a, String b) {
-        StringBuilder stringBuilder = new StringBuilder();
+        StringBuilder result = new StringBuilder();
         int carry = 0;
         int num = Math.max(a.length(), b.length());
         for (int i = 0; i < num; i++) {
-            carry = (i < a.length() ? (a.charAt(a.length() - 1 - i) - '0') : 0) + carry;
-            carry = (i < b.length() ? (b.charAt(b.length() - 1 - i) - '0') : 0) + carry;
-            stringBuilder.append((char) (carry % 2 + '0'));
-            carry = carry / 2;
+            carry += i < a.length() ? (a.charAt(a.length() - 1 - i) - '0') : 0;
+            carry += i < b.length() ? (b.charAt(b.length() - 1 - i) - '0') : 0;
+            result.append(carry % 2);
+            carry /= 2;
         }
         if (carry > 0) {
-            stringBuilder.append('1');
+            result.append(1);
         }
-        stringBuilder.reverse();
-        return stringBuilder.toString();
+        result.reverse();
+        return result.toString();
     }
 }
