cbf184ae7630912a399950363266ad7baa233e6f
wangzhiping <wang921332617@qq.com>
2022-03-19 21:37:10 +0800

    小和

--- src/sort/MergeSort.java
+++ src/sort/MergeSort.java
@@ -7,11 +7,15 @@ public class MergeSort {
     private static int[] aux;
 
     public static void main(String[] args) {
-        int[] arr = {6,2,1,7,3,5,9,52,12,33,624,6,1,2,6,2,13,6,134,6,16,13,61,61,361,361,6};
-        sort(arr);
-        System.out.println(Arrays.toString(arr));
+        int[] arr = {1,2,3,4,5};
+//        sort(arr);
+//        System.out.println(Arrays.toString(arr));
+        System.out.println(smallSum(arr));
     }
 
+    /**
+     * 排序入口
+     */
     public static void sort(int[] arr) {
         aux = new int[arr.length];
         sort(arr, 0, arr.length-1);
@@ -40,5 +44,54 @@ public class MergeSort {
             else if (aux[j] < aux[i]) arr[k] = aux[j++]; // 如果数组右边的数比较小，使用右边的数
             else arr[k] = aux[i++]; // 如果数组左边的数比较小，使用左边的数
         }
+    }
+
+    /**
+     * 求小和入口
+     */
+    public static int smallSum(int[] arr) {
+        if (arr == null || arr.length <= 1) return 0;
+        aux = new int[arr.length];
+        return process(arr, 0, arr.length-1);
+    }
+
+    public static int process(int[] arr, int start, int end) {
+        if (start == end) return 0;
+        int mid = (start + end) / 2;
+        return process(arr, start, mid) + process(arr, mid + 1, end) + mergeAndSmallSum(arr, start, mid, end);
+    }
+
+    /**
+     * 归并排序并求小和
+     * 小和：把每一个数左边比当前数小的数累加起来,叫做这个数组的小和
+     * 比如:[1,2,3,4,5]
+     * 1的左边没有数，小和是：0
+     * 2的左边有1比它小，小和是：1
+     * 3的左边有1，2比它小，小和是：3
+     * 4的左边有1，2，3比它小，小和是6，
+     * 5的左边有1，2，3，4比它小，小和是10，
+     * 所有小和加起来是20
+     *
+     * 基本思路：求小和可以等同为，遍历数组，查看当前值arr[i]右边有多少个数比arr[i]大，
+     * 说明arr[i]就会被记录多少次小和，再把所有数加起来。
+     * 结合归并排序是为了让数不重复计算，也不漏算，且每个数都有机会成为被别的数算为小和的数，或者以这个数为准来算小和。
+     */
+    public static int mergeAndSmallSum(int[] arr, int start, int mid, int end) {
+        int i = start;
+        int j = mid + 1;
+        int result = 0;
+        for (int k = start; k <= end; k++) {
+            aux[k] = arr[k];
+        }
+        for (int k = start; k <= end; k++) {
+            if (i > mid) arr[k] = aux[j++]; // 数组左边已经遍历完，直接使用右边的数
+            else if (j > end) arr[k] = aux[i++]; // 数组右边已经遍历完，直接使用左边的数
+            else if (aux[j] < aux[i]) arr[k] = aux[j++]; // 如果数组右边的数比较小，使用右边的数
+            else {
+                result += aux[i] < aux[j] ? (end-j+1) * aux[i]: 0;
+                arr[k] = aux[i++]; // 如果数组左边的数比较小，使用左边的数
+            }
+        }
+        return result;
     }
 }
