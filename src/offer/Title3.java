7408411f69eea08e0890d808c95f4740876b6d6a
王治平 <wang921332617@qq.com>
2022-06-03 01:48:57 +0800

    code

--- src/offer/Title3.java
+++ src/offer/Title3.java
@@ -11,12 +11,13 @@ package offer;
 public class Title3 {
     public static void main(String[] args) {
         int[] arr = {2,3,1,0,2,5,3};
-        System.out.println(duplicate(arr));
+        System.out.println(duplicate2(arr));
     }
 
     /**
      * 新建一个数组
      * {2,1,3,2}
+     * 空间复杂度O(n)
      */
     public static int duplicate(int[] arr) {
         if (arr == null || arr.length == 0) {
@@ -27,6 +28,30 @@ public class Title3 {
             result[arr[i]]++;
             if (result[arr[i]] >= 2) {
                 return arr[i];
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 空间复杂度O(1)的方法
+     * {2,3,1,0,2,5,3}
+     * 每个数先与自己的下标比较，如果不相等，进入循环，相等跳下一个
+     * 如果下标与下标上的数对应的数组中的数相等，返回这个数（重复了），比如arr[0]=2与arr[2]=2符合条件
+     * 否则交换这两个数，重复上述步骤
+     */
+    public static int duplicate2(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return -1;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            while (arr[i] != i) {
+                if (arr[i] == arr[arr[i]]) {
+                    return arr[i];
+                }
+                int temp = arr[i];
+                arr[i] = arr[temp];
+                arr[temp] = temp;
             }
         }
         return -1;
