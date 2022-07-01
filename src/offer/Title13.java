0bd475fbd1a1e61fadec22c915de30145a43349d
王治平 <wang921332617@qq.com>
2022-07-01 23:45:26 +0800

    offer title 13

---
+++ src/offer/Title13.java
@@ -0,0 +1,38 @@
+package offer;
+
+/**
+ * 机器人的运动范围
+ * 地上有一个m行n列的方格，从坐标 [0,0] 到坐标 [m-1,n-1] 。
+ * 一个机器人从坐标 [0, 0] 的格子开始移动，它每次可以向左、右、上、下移动一格（不能移动到方格外），
+ * 也不能进入行坐标和列坐标的数位之和大于k的格子。例如，当k为18时，机器人能够进入方格 [35, 37] ，
+ * 因为3+5+3+7=18。但它不能进入方格 [35, 38]，因为3+5+3+8=19。请问该机器人能够到达多少个格子？
+ */
+public class Title13 {
+
+    public static void main(String[] args) {
+        movingCount(4,4,3);
+    }
+
+    public static int movingCount(int m, int n, int k) {
+        boolean[][] visited = new boolean[m][n];
+        return dfs(0, 0, m, n, k, visited);
+    }
+
+    public static int dfs(int i, int j, int m, int n, int k, boolean[][] visited) {
+        if (i >= m || j >= n || k < getSum(i) + getSum(j) || visited[i][j]) {
+            return 0;
+        }
+        visited[i][j] = true;
+        return 1 + dfs(i + 1, j, m, n, k, visited) + dfs(i, j + 1, m, n, k, visited);
+    }
+
+    private static int getSum(int a) {
+        int sum = a % 10;
+        int tmp = a / 10;
+        while (tmp > 0) {
+            sum += tmp % 10;
+            tmp /= 10;
+        }
+        return sum;
+    }
+}
