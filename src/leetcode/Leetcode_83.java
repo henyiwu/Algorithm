b7027f8d38d5c95b67efe6a84b8c8cfaf17e3f81
wangzhiping <wangzhiping@inke.cn>
2022-07-12 16:26:34 +0800

    83

---
+++ src/leetcode/Leetcode_83.java
@@ -0,0 +1,61 @@
+package leetcode;
+
+/**
+ * 83. 删除排序链表中的重复元素
+ * 给定一个已排序的链表的头 head ， 删除所有重复的元素，使每个元素只出现一次 。返回 已排序的链表 。
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：head = [1,1,2]
+ * 输出：[1,2]
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：head = [1,1,2,3,3]
+ * 输出：[1,2,3]
+ */
+public class Leetcode_83 {
+
+    public static void main(String[] args) {
+        ListNode listNode1 = new ListNode(1);
+        ListNode listNode2 = new ListNode(1);
+        ListNode listNode3 = new ListNode(1);
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        deleteDuplicates(listNode1);
+    }
+
+    public static ListNode deleteDuplicates(ListNode head) {
+        ListNode temp = head;
+        while (temp != null && temp.next != null) {
+            if (temp.val == temp.next.val) {
+                // 下一个节点与当前节点相同，当前节点指向下下节点
+                temp.next = temp.next.next;
+            } else {
+                // 下一个节点与当前节点不同，遍历下一个节点
+                temp = temp.next;
+            }
+        }
+        return head;
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
+
+
