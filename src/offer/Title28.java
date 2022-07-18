d3657d9513107c0b54ab261cdb3c5ea8c2ebaa4e
王治平 <wang921332617@qq.com>
2022-07-18 23:20:55 +0800

    title 29

---
+++ src/offer/Title28.java
@@ -0,0 +1,57 @@
+package offer;
+
+/**
+ * 请实现一个函数，用来判断一棵二叉树是不是对称的。如果一棵二叉树和它的镜像一样，那么它是对称的。
+ * <p>
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ * <p>
+ *     1
+ *    / \
+ *   2   2
+ *  / \ / \
+ * 3  4 4  3
+ * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+ * <p>
+ *     1
+ *    / \
+ *   2   2
+ *    \   \
+ *    3    3
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：root = [1,2,2,3,4,4,3]
+ * 输出：true
+ * 示例 2：
+ * <p>
+ * 输入：root = [1,2,2,null,3,null,3]
+ * 输出：false
+ */
+public class Title28 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        return root == null || recur(root.left, root.right);
+    }
+
+    boolean recur(TreeNode L, TreeNode R) {
+        if (L == null && R == null) return true;
+        if (L == null || R == null || L.val != R.val) return false;
+        return recur(L.left, R.right) && recur(L.right, R.left);
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
---
+++ src/offer/Title29.java
@@ -0,0 +1,81 @@
+package offer;
+
+/**
+ * 输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：matrix = [[1,2,3],[4,5,6],[7,8,9]]
+ * 输出：[1,2,3,6,9,8,7,4,5]
+ * 示例 2：
+ * <p>
+ * 输入：matrix =[[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+ * 输出：[1,2,3,4,8,12,11,10,9,5,6,7]
+ * <p>
+ * 限制：
+ * <p>
+ * 0 <= matrix.length <= 100
+ * 0 <= matrix[i].length <= 100
+ */
+public class Title29 {
+    public static void main(String[] args) {
+        int[][] arr = {};
+        spiralOrder(arr);
+    }
+
+    public static int[] spiralOrder(int[][] matrix) {
+        int direction = 4; // 1 2 3 4 上下左右
+        if (matrix == null || matrix.length == 0) {
+            return new int[0];
+        }
+        boolean[][] bArr = new boolean[matrix.length][matrix[0].length];
+        int length = matrix.length * matrix[0].length;
+        int[] result = new int[length];
+        int row = 0;
+        int column = 0;
+        for (int i = 0; i < length; i++) {
+            result[i] = matrix[row][column];
+//            System.out.println("row " + row + ",, column " + column);
+            if (direction == 4) { // 右
+                if (column + 1 < matrix[0].length && !bArr[row][column + 1]) {
+                    bArr[row][column] = true;
+                    column++;
+                } else {
+                    direction = 2;
+                    row++;
+                }
+                continue;
+            }
+            if (direction == 2) { // 下
+                if (row + 1 < matrix.length && !bArr[row + 1][column]) {
+                    bArr[row][column] = true;
+                    row++;
+                } else {
+                    column--;
+                    direction = 3;
+                }
+                continue;
+            }
+            if (direction == 3) { // 左
+                if (column - 1 >= 0 && !bArr[row][column - 1]) {
+                    bArr[row][column] = true;
+                    column--;
+                } else {
+                    row--;
+                    direction = 1;
+                }
+                continue;
+            }
+            if (direction == 1) { // 上
+                if (row - 1 >= 0 && !bArr[row - 1][column]) {
+                    bArr[row][column] = true;
+                    row--;
+                } else {
+                    column++;
+                    direction = 4;
+                }
+            }
+        }
+        return result;
+    }
+}
