18b565a8113b195e9cc580e09bc2468ae67f60ab
wangzhiping <wangzhiping@inke.cn>
2022-07-21 18:10:02 +0800

    26

---
+++ src/offer/Title26.java
@@ -0,0 +1,56 @@
+package offer;
+
+/**
+ * 输入两棵二叉树A和B，判断B是不是A的子结构。(约定空树不是任意一个树的子结构)
+ * <p>
+ * B是A的子结构， 即 A中有出现和B相同的结构和节点值。
+ * <p>
+ * 例如:
+ * 给定的树 A:
+ * <p>
+ *     3
+ *   / \
+ *  4  5
+ *  / \
+ * 1   2
+ * 给定的树 B：
+ * <p>
+ *   4
+ *  /
+ * 1
+ * 返回 true，因为 B 与 A 的一个子树拥有相同的结构和节点值。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：A = [1,2,3], B = [3,1]
+ * 输出：false
+ * 示例 2：
+ * <p>
+ * 输入：A = [3,4,5,1,2], B = [4,1]
+ * 输出：true
+ * 限制：
+ * <p>
+ * 0 <= 节点个数 <= 10000
+ */
+public class Title26 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    class Solution {
+        public boolean isSubStructure(TreeNode A, TreeNode B) {
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
