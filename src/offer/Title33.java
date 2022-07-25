e2b106abc2c41d2f6a8a37f0dc976983b7fde729
wangzhiping <wangzhiping@inke.cn>
2022-07-25 17:27:34 +0800

    title 33 注解

--- src/offer/Title33.java
+++ src/offer/Title33.java
@@ -27,19 +27,37 @@ public class Title33 {
             return recur(postorder, 0, postorder.length - 1);
         }
 
+        /**
+         *
+         * [5, 2, 6, 1, 3]
+         *
+         *      5
+         *     / \
+         *    2   6
+         *   / \
+         *  1   3
+         *
+         *  input : [1,3,2,6,5]
+         *
+         *  以下解法假设输入的数组符合规则
+         */
         boolean recur(int[] postorder, int i, int j) {
             if (i >= j) return true;
             int p = i;
+            // 找到根节点的右子节点
             while (postorder[p] < postorder[j]) p++;
             int m = p;
+            // postorder[j]是根节点，postorder[p]是根节点右边的所有节点，一定比根节点大
             while (postorder[p] > postorder[j]) p++;
+            // 如果是二叉搜索树的后序遍历，p一定和j相等
+            // 再遍历根节点左边的节点 和 根节点右边的节点
             return p == j && recur(postorder, i, m - 1) && recur(postorder, m, j - 1);
         }
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] arr = {1, 3, 2, 13, 15, 7, 9};
+        int[] arr = {1,3,2,6,5};
         System.out.println(solution.verifyPostorder(arr));
     }
 }
