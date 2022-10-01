c0f7253e3f0aa20f66cb3d1a9c5ebc499fb05383
wangzhiping <wangzhiping@inke.cn>
2022-10-01 20:06:47 +0800

    title 56

--- src/offer/Ttitle56.java
+++ src/offer/Ttitle56.java
@@ -27,34 +27,33 @@ public class Ttitle56 {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] arr = {1,2,10,4,1,4,3,3};
+        int[] arr = {1, 2, 10, 4, 1, 4, 3, 3};
         int[] ints = solution.singleNumbers(arr);
         System.out.println(Arrays.toString(ints));
     }
 
     static class Solution {
         public int[] singleNumbers(int[] nums) {
-            // 1.将所有数字异或，得到a与b异或的结果
-            int ret = 0;
-            for (int n : nums) {
-                ret ^= n;
+            // 1. 遍历数组，异或所有数字，得到一个异或结果
+            int temp = 0;
+            for (int num : nums) {
+                temp = temp ^ num;
             }
 
-            // 从地位开始找，找到一位是1，其他都是0的数字
-            // 比如两数异或的结果为8,则应该得到数1000，其中1这个位，能保证结果的两个数一定有一个是1一个是0
-            int div = 1;
-            while ((div & ret) == 0) {
-                div = div << 1;
+            // 2. 从低位往高位取，得到一个第一位是1，其他都是0的数字
+            int value = 1;
+            while ((value & temp) == 0) {
+                value = value << 1;
             }
 
-            // 获得结果
-            int a = 0, b = 0;
-            for (int n : nums) {
-                // 与操作，两个都是1才是1，所以这里只可能有一个数字进if
-                if ((div & n) != 0) {
-                    a ^= n;
+            // 3. 将所有数字与步骤2得到的数字进行与操作，将数组分开
+            int a = 0;
+            int b = 0;
+            for (int num : nums) {
+                if ((num & value) != 0) {
+                    a = a ^ num;
                 } else {
-                    b ^= n;
+                    b = b ^ num;
                 }
             }
             return new int[]{a, b};
