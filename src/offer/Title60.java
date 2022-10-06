8b548a5cc0b0db7c6982fc9c0987eb71253fccf2
wangzhiping <wangzhiping@inke.cn>
2022-10-06 12:07:53 +0800

    骰子，优化版本

--- src/offer/Title60.java
+++ src/offer/Title60.java
@@ -1,8 +1,5 @@
 package offer;
 
-import java.util.Arrays;
-import java.util.HashMap;
-
 /**
  * 剑指 Offer 60. n个骰子的点数
  * 把n个骰子扔在地上，所有骰子朝上一面的点数之和为s。输入n，打印出s的所有可能的值出现的概率。
@@ -25,35 +22,41 @@ import java.util.HashMap;
 public class Title60 {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        double[] doubles = solution.dicesProbability(3);
+        double[] doubles = solution.dicesProbability(2);
         System.out.println();
     }
 
     static class Solution {
         public double[] dicesProbability(int n) {
-            int[][] dp = new int[n + 1][n * 6 + 1];
-            for (int i = 1; i <= 6; i++) {
-                dp[1][i] = 1;
+            // 1. 把一个骰子所有面出现的个数，记录在二维数组中
+            int[][] arr = new int[n][n * 6];
+            for (int i = 0; i < 6; i++) {
+                arr[0][i] = 1;
             }
-            for (int i = 2; i <= n; i++) {
-                for (int j = i; j <= 6 * i; j++) {
-                    System.out.println("-----------");
-                    for (int k = 1; k <= 6; k++) {
-                        if (j < k) break;
-                        System.out.println("dp[" + i + "][" + j + "] += dp[" + (i - 1) + "][" + (j - k) + "]");
-                        dp[i][j] += dp[i - 1][j - k];
+
+            // 2. 填满二维数组，arr[i][j]表示，i+1个骰子，出现j+1点数的次数是多少
+            for (int i = 1; i < n; i++) {
+                for (int j = 0; j < n * 6; j++) {
+                    for (int k = 0; k < 6; k++) {
+                        if ((j - k - 1) < 0) {
+                            break;
+                        }
+                        arr[i][j] = arr[i][j] + arr[i - 1][j - k - 1];
                     }
-                    System.out.println("-----------");
                 }
             }
 
-            double[] res = new double[5 * n + 1];
+            // 3.创建结果数组，将点数转换成概率
+            double[] result = new double[n * 6 - (n - 1)];
             int index = 0;
-            double sum = Math.pow(6, n);
-            for (int i = n; i <= 6 * n; i++) {
-                res[index++] = dp[n][i] / sum;
+            for (int i = 0; i < arr[n - 1].length; i++) {
+                if (arr[n - 1][i] == 0) {
+                    continue;
+                }
+                result[index++] = arr[n - 1][i] / Math.pow(6, n);
             }
-            return res;
+
+            return result;
         }
     }
 }
