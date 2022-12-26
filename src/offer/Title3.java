ac42fa5f82209aa3e6c4bbb6787af2588273a6bf
王治平 <wang921332617@qq.com>
2022-12-27 00:19:46 +0800

    title 3

--- src/offer/Title3.java
+++ src/offer/Title3.java
@@ -1,5 +1,7 @@
 package offer;
 
+import java.util.Arrays;
+
 /**
  * 数组中的重复数字
  * 在一个长度位n的数组里的所有数字都在0~n-1的范围内。
@@ -10,7 +12,7 @@ package offer;
 
 public class Title3 {
     public static void main(String[] args) {
-        int[] arr = {2,3,1,0,2,5,3};
+        int[] arr = {3, 4, 2, 1, 1, 0};
         System.out.println(duplicate2(arr));
     }
 
@@ -35,17 +37,29 @@ public class Title3 {
 
     /**
      * 空间复杂度O(1)的方法
-     * {2,3,1,0,2,5,3}
-     * 每个数先与自己的下标比较，如果不相等，进入循环，相等跳下一个
-     * 如果下标与下标上的数对应的数组中的数相等，返回这个数（重复了），比如arr[0]=2与arr[2]=2符合条件
-     * 否则交换这两个数，重复上述步骤
+     * 思路：从头遍历，把当前a[i]和a[a[i]]的数字交换，直到a[i]=i，如果碰巧a[i]=a[a[i]]，这个数就是结果
+     * <p>
+     * 测试数据： [2, 3, 1, 0, 2, 5, 3]
+     * loop0:   [1, 3, 2, 0, 2, 5, 3]
+     * [3, 1, 2, 0, 2, 5, 3]
+     * [0, 1, 2, 3, 2, 5, 3]   i=0位置一定是0
+     * -----------------------
+     * loop1:   [0, 1, 2, 3, 2, 5, 3]   i=1位置一定是1
+     * -----------------------
+     * loop2:   [0, 1, 2, 3, 2, 5, 3]   i=2位置一定是2
+     * -----------------------
+     * loop3:   [0, 1, 2, 3, 2, 5, 3]   i=3位置一定是3
+     * -----------------------
+     * loop4:   [0, 1, 2, 3, 2, 5, 3]   i=4位置刚好是i[i[4]]
+     * return 2
      */
+    // {3, 4, 2, 1, 1, 0}
     public static int duplicate2(int[] arr) {
         if (arr == null || arr.length == 0) {
             return -1;
         }
         for (int i = 0; i < arr.length; i++) {
-            while (arr[i] != i) {
+            while (i != arr[i]) {
                 if (arr[i] == arr[arr[i]]) {
                     return arr[i];
                 }
