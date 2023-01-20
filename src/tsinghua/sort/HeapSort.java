09af115c3cbdfc20b8f9214182cbcf5442fe3685
王治平 <wang921332617@qq.com>
2023-01-20 10:37:49 +0800

    heap sort

--- src/tsinghua/sort/HeapSort.java
+++ src/tsinghua/sort/HeapSort.java
@@ -31,53 +31,39 @@ public class HeapSort {
     }
 
     public static void heapSort(int[] arr) {
-        //为什么从arr.length/2-1开始？
+        // 初始化构建堆
         for (int i = arr.length / 2 - 1; i >= 0; i--) {
-            adjustHeap(arr, i, arr.length);
+            heapAdjust(arr, i, arr.length);
         }
-
-        for (int j = arr.length - 1; j > 0; j--) {
-            int temp = arr[j];
-            arr[j] = arr[0];
-            arr[0] = temp;
-            /*为什么从0开始？
-                因为在第一次构建大顶堆后让堆顶元素和末尾元素进行交换
-                而对于其他的非叶子结点所对应的子树都是大顶堆就无需调整，
-                只需要堆顶元素(下标为0的非叶子结点)的子树调整成大顶堆
-            */
-            adjustHeap(arr, 0, j);
-
+        // 把第一个数和最后一个数交换，构建堆
+        for (int i = arr.length - 1; i >= 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            heapAdjust(arr, 0, i);
         }
     }
 
     /**
-     * 构建大顶堆
-     * 注意：
-     * 这个方法并不是将整个树调整成大顶堆
-     * 而是以i对应的非叶子结点的子树调整成大顶堆
-     *
-     * @param arr    待调整的数组
-     * @param i      非叶子结点在数组中的索引(下标)
-     * @param length 进行调整的元素的个数，length是在逐渐减少的
+     * 堆调整
+     * @param arr 数组
+     * @param low 根节点下标
+     * @param length 需要调整的数组长度
      */
-    public static void adjustHeap(int[] arr, int i, int length) {
-        /*取出当前非叶子结点的值保到临时变量中*/
-        int temp = arr[i];
-
-        /*j=i*2+1表示的是i结点的左子结点*/
-        for (int k = i * 2 + 1; k < length; k = k * 2 + 1) {
-            if (k + 1 < length && arr[k] < arr[k + 1]) { //左子结点小于右子结点
-                k++; //j指向右子结点
+    public static void heapAdjust(int[] arr, int low, int length) {
+        int root = arr[low];
+        for (int i = low * 2 + 1; i < length; i = i * 2 + 1) {
+            if (i + 1 < length && arr[i] < arr[i + 1]) { //如果存在右孩子，且右孩子比左孩子大
+                i++; // 指向右孩子
             }
-            if (arr[k] > temp) { //子节点大于父节点
-                arr[i] = arr[k]; //把较大的值赋值给父节点
-                //arr[j] = temp; 这里没必要换
-                i = k; //让i指向与其换位的子结点 因为
+            if (arr[i] > root) { // 如果子节点比根节点大
+                arr[low] = arr[i]; // 把子节点的数值赋值给根节点
+                low = i; // low指向子节点
             } else {
-                /*子树已经是大顶堆了*/
                 break;
             }
         }
-        arr[i] = temp;
+        // 在循环中，被移动的数，都比root大，把root赋值给low
+        arr[low] = root;
     }
 }
